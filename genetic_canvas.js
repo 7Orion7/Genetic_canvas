1fb25d716c9d99ddf6cc7e8ad0f016812c053f1d
Xaver <xaver.fabian@gmail.com>
2017-04-26 22:06:02 +0200

    First commit

---
+++ genetic_canvas.js
@@ -0,0 +1,5 @@
+// Uses a genetic algorithm to transform a canvas to a picture similar to this
+//http://jsfiddle.net/mankyd/X6x9L/9/
+// need to read node js
+//maybe do that tomorrow
+// should use git for it
